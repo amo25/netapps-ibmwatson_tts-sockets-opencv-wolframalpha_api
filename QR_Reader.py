49a7faecc754dd7c0f22c73b379ee0c8f61badb9
Alex O <amo25@vt.edu>
2019-03-01 13:01:28 -0500

    Created a script to install OpenCV and it's dependencies. Camera integration with OpenCV QR code reader tested and working (though can be optimized for speed if necessary). Ready for integration

--- QR_Reader.py
+++ QR_Reader.py
@@ -1,14 +1,29 @@
 import pyzbar.pyzbar as pyzbar
-import numpy as np
-import cv2
 
-# Read image. TODO link to camera
-im = cv2.imread('Alex_QR_Code.png')
+import numpy as np
+
+import cv2
+import subprocess
+
+#take an image
+subprocess.call(["raspistill", "-o", "Camera_QR_Code.jpg"])
+
+# Read image.
+
+im = cv2.imread('Camera_QR_Code.jpg')
+
+
 
 #Find qr codes and barcodes in the image
+
 decodedObjects = pyzbar.decode(im)
 
+
+
 # Print results. #TODO remove print and send object data to the next thing. Maybe use obj.type for error checking
+
 for obj in decodedObjects:
+
     print('Type : ', obj.type)
+
     print('Data : ', obj.data, '\n')
